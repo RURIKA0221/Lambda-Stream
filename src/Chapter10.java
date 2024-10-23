fea0cf2de0a04be28150a0ffebe09820c4896f23
Rurika Fuyuki <181914382+RURIKA0221@users.noreply.github.com>
2024-10-23 19:43:33 +0900

    課題再提出

--- src/Chapter10.java
+++ src/Chapter10.java
@@ -3,22 +3,17 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class Chapter10 {
+	public static void main(String[] args) throws Exception {
+		List<Task> list = new ArrayList<>();
+		list.add(new Task(LocalDate.of(2021, 10, 21), "牛乳を買う", true));
+		list.add(new Task(LocalDate.of(2021, 9, 15), "○○社面接", false));
+		list.add(new Task(LocalDate.of(2021, 12, 4), "手帳を買う", true));
+		list.add(new Task(LocalDate.of(2021, 8, 10), "散髪に行く", false));
+		list.add(new Task(LocalDate.of(2021, 11, 9), "スクールの課題を解く", false));
 
-		public static void main(String[] args) throws Exception {
-	        List<Task> list = new ArrayList<>();
-	        list.add(new Task(LocalDate.of(2021, 10, 21), "牛乳を買う", true));
-	        list.add(new Task(LocalDate.of(2021, 9, 15), "○○社面接", false));
-	        list.add(new Task(LocalDate.of(2021, 12, 4), "手帳を買う", true));
-	        list.add(new Task(LocalDate.of(2021, 8, 10), "散髪に行く", false));
-	        list.add(new Task(LocalDate.of(2021, 11, 9), "スクールの課題を解く", false));
-
-	        // 以下記述
-	      Long c = list.stream().filter(f -> f.isDone() == false).count(); //未完了のタスクの個数をcount
-	      System.out.println("未完了のタスクの個数は" + c);
-	      System.out.println("【未完了のタスクを昇順に並び替えて一覧表示】");
-	      list.stream() 
-	      .filter(f -> f.isDone() == false)
-	      .sorted((f1, f2) -> f1.compareTo(f2))
-	      .forEach(System.out::println);
-	    }
+		Long c = list.stream().filter(f -> f.isDone() == false).count(); 
+		System.out.println("未完了のタスクの個数は" + c);
+		System.out.println("【未完了のタスクを昇順に並び替えて一覧表示】");
+		list.stream().filter(f -> f.isDone() == false).sorted().forEach(System.out::println);
+	}
 }
